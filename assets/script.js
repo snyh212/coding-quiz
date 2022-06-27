c8af409b15f4474a15987ca082806a51ef75f0a2
Shmuel Hoffman <snyh212@gmail.com>
2022-06-26 22:54:31 -0700

    folder and files made, base html and js outline added

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+/* start button */
+/* timer */
+/* question */
+/* if right next  q */
+/* if wrong deduct time */
+/* record how much time it took */
+/* when done or time out game over */
+/* save score */
